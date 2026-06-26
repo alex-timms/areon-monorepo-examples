de21ec102842773fceedcba8de48c801258f21fe
Alex Timms <alexander.timms@sainsburys.co.uk>
2026-06-26 16:38:46 +0100

    [AJT] temp add changes

--- ipc-shared/src/main/java/ipc/mediaDriver/shared/Main.java
+++ ipc-shared/src/main/java/ipc/mediaDriver/shared/Main.java
@@ -5,12 +5,12 @@ import io.aeron.Publication;
 import io.aeron.Subscription;
 import io.aeron.driver.MediaDriver;
 import io.aeron.driver.ThreadingMode;
+import ipc.agents.ReceiveAgent;
+import ipc.agents.SendAgent;
 import org.agrona.concurrent.AgentRunner;
 import org.agrona.concurrent.BusySpinIdleStrategy;
 import org.agrona.concurrent.IdleStrategy;
 import org.agrona.concurrent.ShutdownSignalBarrier;
-import ajt.agrona.custom.agents.ReceiveAgent;
-import ajt.agrona.custom.agents.SendAgent;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
